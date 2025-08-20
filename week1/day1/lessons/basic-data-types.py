d466213f164a1a7033f8681f5bb0890e6084c6f6
ju05 <j.amaralwork@gmail.com>
2025-08-20 12:34:25 +0300

    first commit

---
+++ week1/day1/lessons/basic-data-types.py
@@ -0,0 +1,105 @@
+#BASIC VALUE TYPES
+
+# #STRINGS
+# my_name = "Juliana"
+
+# #check the lenght
+# print(len('Juliana'))
+
+# #access chars by index (indexes start from 0)
+# print(my_name[4])
+
+# my_name = 'John snow'
+
+# #they are immutable 
+
+# # my_name[0] = 'G'
+
+# #String functions/ String Methods
+
+# print(my_name.lower())
+# print(my_name.upper())
+# print(my_name.capitalize())
+# print(my_name.title())
+
+# student = 'Harry Potter'
+# student2 = student.replace('r', 'i')
+# print(student2)
+
+# price = '15$'
+
+# clean_price = price.strip('$')
+# print(clean_price)
+
+#exercise in class
+# description = "strings are..."
+# print(description.upper())
+# description = description.replace('are', 'is')
+# print(description)
+# print(description[:7])
+
+# #Numbers
+# #integers (int)
+# age_int = 35
+# age_int += 5
+# print(age_int)
+# print(7 + (-5))
+# print(10 / 2)
+# print(10 % 3)
+
+# #floats
+# print(3.5 + 3)
+# division = 10 / 3
+# print(round(division, 2))
+
+# # Type Casting
+
+# age = int(input('how old are you?'))
+
+# print(type(age))
+
+# print(age + 10)
+
+# height = float(input('What is your height?'))
+
+# print(type(height))
+
+# age = str(age)
+# print(type(age))
+
+
+#Boleans (True and False)
+
+print(5 > 7)
+print('5' == 5)
+print(-1 != 1)
+
+
+#General Useful knowledge
+
+#Adding types
+
+my_string = 'Hello World '
+python = 'Python is fun'
+print(my_string * 5)
+
+#special characters
+print('Hello, aren\'t you having fun?\n' * 5)
+print('Hello, aren\'t you having fun?\t' * 5)
+
+#the f' string
+
+user_name = input('What\'s your name?')
+day_week = 'Wednesday'
+
+print('Welcome, <user_name>!')
+print('Welcome, ' + user_name + '!')
+print(f'Welcome, {user_name}! Today is {day_week}')
+
+# Create a variable called first_name and a variable called last_name, and then print your full name using those two variables
+
+first_name = 'Hermione'
+last_name = 'Granger'
+
+print(f'Hello, my name is {first_name} {last_name}')
+print(first_name, last_name)
---
+++ week1/day1/lessons/conditionals.py
@@ -0,0 +1,36 @@
+#CONDITIONALS
+
+#SYNTAX
+#if <condition>:
+#(indented block) <action>
+
+# if 5 < 3:
+#     print('Hello there!')
+
+# user_num = int(input('Guess a number'))
+# secret_num = 18
+
+# if user_num == secret_num:
+#     print('Congrats, you won!')
+
+# elif user_num == 7:
+#     print('Thats my lucky number!')
+
+# elif user_num < 1: 
+#     print('Don\'t enter negative numbers')
+
+# else:
+#     print('Wrong number, try again')
+
+#Exercise in class
+# user_name = input('Please enter your name:')
+
+# if len(user_name) <= 5:
+#     print('You have a short name')
+# else:
+#     print('You have a long name')
+
+my_hobbies = "sport, code, food, icecreams, netflix"
+
+if 'yoga' in my_hobbies or 'soccer' in my_hobbies:
+    print('Let\'s have medidation!')
